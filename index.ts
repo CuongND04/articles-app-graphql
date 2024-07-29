7a805c24e505496ff017c814165749e99e745d5b
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-29 08:19:48 +0700

    ✅ Kết nối Database

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import express, { Express, Request, Response } from "express";
-
+import * as database from "./config/database";
+import dotenv from "dotenv";
+import Article from "./models/article.model";
 const app: Express = express();
-const port: number = 3000;
+const port: string | number = process.env.PORT || 3000;
 
+dotenv.config(); // cái này phải đặt trên connect
+database.connect();
 // Rest API
-app.get("/articles", (req: Request, res: Response) => {
+app.get("/articles", async (req: Request, res: Response) => {
+  const article = await Article.find({
+    deleted: false,
+  });
   res.json({
-    articles: [],
+    articles: article,
   });
 });
 
